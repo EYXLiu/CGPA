966f3e8f2029c895da870b81e9a7a23150579905
Evan Liu <evanyxliu@gmail.com>
2025-04-13 14:26:16 -0400

    a

--- src/main/java/com/CGPA/CgpaApplication.java
+++ src/main/java/com/CGPA/CgpaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CgpaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CgpaApplication.class, args);
 	}
