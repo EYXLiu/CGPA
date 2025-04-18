39cdac2d6934074055d3b0571ed30dea53aa7405
Evan Liu <evanyxliu@gmail.com>
2025-04-17 22:46:55 -0400

    rm unneeded

--- src/main/java/com/CGPA/util/JwtUtil.java
+++ src/main/java/com/CGPA/util/JwtUtil.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
